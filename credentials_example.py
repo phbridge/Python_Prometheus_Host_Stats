ec02def518a1258dbbd409da37c1f887d17c6589
phbridge <bridges.p.j1@gmail.com>
2021-01-30 02:20:56 +0000

    added modules to run as push in thread vs pull only fixed typo

--- credentials_example.py
+++ credentials_example.py
@@ -3,3 +3,4 @@ FLASK_PORT = 8050
 FLASK_HOSTNAME = "example.com"
 ABSOLUTE_PATH = "/Users/phbridge/Documents/Git/Python_Prometheus_Host_Stats"
 LOGFILE = ABSOLUTE_PATH + "/logs/Python_Prometheus_Host_Stats_%s_%s.log" % (FLASK_HOST, FLASK_PORT)
+INFLUX_DB_PATH = "https://influx.com"
