70dfdf10b0a9fe798f61397de6c7f908344f744d
phbridge <bridges.p.j1@gmail.com>
2021-02-02 10:31:18 +0000

    updated credentials_example.py and service.example

--- credentials_example.py
+++ credentials_example.py
@@ -1,7 +1,7 @@
 FLASK_HOST = "::"
 FLASK_PORT = 8050
 FLASK_HOSTNAME = "example.com"
-ABSOLUTE_PATH = "/Users/phbridge/Documents/Git/Python_Prometheus_Host_Stats"
+ABSOLUTE_PATH = "/Users/XXXXXXXX/Documents/Git/Python_Prometheus_Host_Stats"
 LOGFILE = ABSOLUTE_PATH + "/logs/Python_Prometheus_Host_Stats_%s_%s.log" % (FLASK_HOST, FLASK_PORT)
 INFLUX_MODE = True
 FLASK_MODE = False
