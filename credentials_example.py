48c79deede938b330efb929450c0136b61f8f989
phbridge <bridges.p.j1@gmail.com>
2021-02-02 10:30:42 +0000

    updated credentials_example.py and service.example

--- credentials_example.py
+++ credentials_example.py
@@ -3,6 +3,6 @@ FLASK_PORT = 8050
 FLASK_HOSTNAME = "example.com"
 ABSOLUTE_PATH = "/Users/phbridge/Documents/Git/Python_Prometheus_Host_Stats"
 LOGFILE = ABSOLUTE_PATH + "/logs/Python_Prometheus_Host_Stats_%s_%s.log" % (FLASK_HOST, FLASK_PORT)
-INFLUX_DB_PATH = "https://influx.com"
 INFLUX_MODE = True
 FLASK_MODE = False
+INFLUX_DB_PATH = ["https://influx-pri.test.co.uk:8086/write?db=<DBNAME>&u=<UserName>&p=<Password>","https://influx-sec.test.co.uk:8086/write?db=<DBNAME>&u=<UserName>&p=<Password>"]
